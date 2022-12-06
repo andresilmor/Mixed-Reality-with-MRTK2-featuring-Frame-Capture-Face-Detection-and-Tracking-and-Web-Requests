1b07f0eba582da442fea8d70c7cc6de144836804
André Moreira <andresilmor@outlook.com>
2022-12-06 11:52:35 +0000

    TRF

--- Assets/UnityProject/Scripts/Scriptable Objects/GraphicUserInterfaceScriptableObject.cs
+++ Assets/UnityProject/Scripts/Scriptable Objects/GraphicUserInterfaceScriptableObject.cs
@@ -1,20 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Microsoft.MixedReality.Toolkit.Input.KeyBinding;
 
 [CreateAssetMenu(menuName = "Scriptable Object/Graphic User Interface")]
 public class GraphicUserInterfaceScriptableObject : ScriptableObject
 {
 
     [System.Serializable]
-    public struct data
+    public class data
     {
         public string name;
         public GameObject window;
-        public string[] windowComponents;
+        public List<windowComponents> components = new List<windowComponents>();
+        public Dictionary<string, string> componentsDict = new Dictionary<string, string>();
     }
 
-
+    [System.Serializable]
+    public struct windowComponents
+    {
+        public string name;
+        public componentType type;
+        public string path;
+    }
 
 
 
@@ -25,5 +34,24 @@ public class GraphicUserInterfaceScriptableObject : ScriptableObject
         {
             return _windows;
         }
+    }
+
+    [SerializeField]
+    public enum componentType
+    {
+        Text,
+        Button
+    }
+
+
+    public void SetupComponentsDictionary() { 
+        foreach (var i in windows) {
+            foreach (var n in i.components) {
+                i.componentsDict.Add(n.name, n.path);
+
+            }
+
+        }
+
     }
 }
